ef44c86819a99e77a501782e011de1ce49855500
elen <elena.sergeeva.99@list.ru>
2022-04-03 09:40:30 +0300

    added some comments

--- Task 1/checkDate.js
+++ Task 1/checkDate.js
@@ -1,16 +1,18 @@
 function checkDate(timestamp) {
+    //заменила множественное создание объекта на единственное в целях оптимизации
     const timestampToDate = new Date(timestamp * 1000);
+    //попробовала исправить проблему с timezones
     const day = timestampToDate.getUTCDate();
     const month = timestampToDate.getUTCMonth();
     const year = timestampToDate.getUTCFullYear();
     const hour = timestampToDate.getUTCHours();
-
+    //немного поменяла именование переменных для более аккуратного вида
     const currentDate = new Date(Date.now());
     const currentDay = currentDate.getUTCDate();
     const currentMonth = currentDate.getUTCMonth();
     const currentYear = currentDate.getUTCFullYear();
+    //заменила блок if на простое присваивание
     let isSameDate = year === currentYear && month === currentMonth && day === currentDay;
-
 
     return {
         isSameDate: isSameDate,
