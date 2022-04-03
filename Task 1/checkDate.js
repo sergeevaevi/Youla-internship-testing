27a5ec24d2895d0ddbb8ee7bd7439e5322438ec3
elen <elena.sergeeva.99@list.ru>
2022-04-03 09:35:32 +0300

    solved problem with date

--- Task 1/checkDate.js
+++ Task 1/checkDate.js
@@ -1,28 +1,20 @@
 function checkDate(timestamp) {
-    var day = new Date(timestamp * 1000).getDate();
-    var month = new Date(timestamp * 1000).getMonth();
-    var year = new Date(timestamp * 1000).getFullYear();
-    var hour = new Date(timestamp * 1000).getHours();
+    const timestampToDate = new Date(timestamp * 1000);
+    const day = timestampToDate.getUTCDate();
+    const month = timestampToDate.getUTCMonth();
+    const year = timestampToDate.getUTCFullYear();
+    const hour = timestampToDate.getUTCHours();
 
-    const current_Date = new Date(Date.now());
-    const current_day = current_Date.getDate();
-    const current_month = current_Date.getMonth() + 1;
-    const currentYear = current_Date.getFullYear();
+    const currentDate = new Date(Date.now());
+    const currentDay = currentDate.getUTCDate();
+    const currentMonth = currentDate.getUTCMonth();
+    const currentYear = currentDate.getUTCFullYear();
+    let isSameDate = year === currentYear && month === currentMonth && day === currentDay;
 
-    let isSameDate = false;
-
-    if (year == currentYear) {
-        if (month == current_month) {
-            if (day == current_day) {
-                isSameDate = true;
-            } else {
-                isSameDate = false;
-            }
-        }
-    }
 
     return {
         isSameDate: isSameDate,
         dayPeriod: hour > 11 ? 'pm' : 'am'
     }
 }
+
